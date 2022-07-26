51e7846d0d67524e93421fe142339c9805ccf270
atlan7172 <atlan_serik@mail.ru>
2022-07-26 15:17:28 +0600

    first commit

---
+++ src/UI/AnotherPost.tsx
@@ -0,0 +1,14 @@
+const AnotherPost = () => {
+    return (
+        <div className='samePosts'>
+        <div className='container'>
+        <h4 className='samePosts__title'>Похожие посты</h4>
+    <div className='samePosts__inner'>
+
+        </div>
+        </div>
+        </div>
+)
+}
+
+export default AnotherPosts
